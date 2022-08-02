6f5faea7984ca39528b0da7074f5fede7ab08b56
Valentyn <valentyncherkasov24@gmail.com>
2022-08-02 12:52:04 +0300

    Adds type indentification by first byte of client request

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ from collections import namedtuple
 from io import BytesIO
 from socket import error as socket_error
 
+from setuptools import Command
+
 
 # Exceptions to notify theconnection-handling loop of problems
 class CommandError(Exception):
@@ -20,9 +22,56 @@ Error = namedtuple("Error", ("message",))
 
 
 class ProtocolHandler(object):
+    def __init__(self):
+        self.handlers = {
+            "+": self.handle_simple_string,
+            "-": self.handle_error,
+            ":": self.handle_integer,
+            "$": self.handle_string,
+            "*": self.handle_array,
+            "%": self.handle_dict,
+        }
+
     def handle_request(self, socket_file):
         """Parses a request from the client into it's component parts"""
-        pass
+        first_request_byte = socket_file.read(1)
+
+        if not first_request_byte:
+            raise Disconnect()
+
+        try:
+            # Delegate to the appropriate handler based on the first byte
+            return self.handlers[first_request_byte](socket_file)
+        except KeyError:
+            raise CommandError("Bad Request")
+
+    def handle_simple_string(self, socket_file):
+        return socket_file.readline().rstrip("\r\n")
+
+    def handle_error(self, socket_file):
+        return Error(socket_file.readline().rstrip("\r\n"))
+
+    def handle_integer(self, socket_file):
+        return int(socket_file.readline().rstrip("\r\n"))
+
+    def handle_string(self, socket_file):
+        # First read the length ($<length>\r\n)
+        length = int(socket_file.readline().rstrip("\r\n"))
+        if length == -1:
+            return None
+        length += 2  # Include the trailing \r\n in count
+        return socket_file.read(length[:-2])
+
+    def handle_array(self, socket_file):
+        number_of_elements = int(socket_file.readline().rstrip("\r\n"))
+        return [self.handle_request(socket_file for _ in range(number_of_elements))]
+
+    def handle_dict(self, socket_file):
+        number_of_items = int(socket_file.readline().rstrip("\r\n"))
+        elements = [
+            self.handle_request(socket_file) for _ in range(number_of_items * 2)
+        ]
+        return dict(zip(elements[::2], elements[1::2]))
 
     def write_response(self, socket_file, data):
         """Serializes the response data and sends it to the client"""
